5a83de2f0dc0b00fce765647737c1ad92d296737
Maksim Khalodny <coldmaxxxx@gmail.com>
2021-07-24 02:58:14 +0300

    fix remains

--- script.js
+++ script.js
@@ -1,23 +1,86 @@
 
-const PRECISION = 10_000;
 
-const exRate = {
+
+const exRateUSD = {
+  'USD':1, 
+  'EUR':0.8493,
+  'PLN':3.8854, 
+  'BYN':2.5136,
+};  
+
+const exRateEUR = {
   'USD':1.1808, 
-  'EUR':1, 
+  'EUR':1,
   'PLN':4.5811, 
   'BYN':2.9919,
 };                                                                      
 
-const Amount = document.getElementById('amount').value
+const exRatePLN = {
+  'USD':0.2574, 
+  'EUR':0.2186,
+  'PLN':1, 
+  'BYN':0.6469,
+};  
 
-const currentTo = document.getElementById("currencyFrom").value
+const exRateBYN = {
+  'USD':0.3978, 
+  'EUR':0.3379,
+  'PLN':1.5458, 
+  'BYN':1,
+};  
 
-const currentFrom  = document.getElementById("currencyTo").value
-console.log(Amount)
-function convertAmount () {
-document.getElementById("result").innerHTML = Amount
-console.log(Amount)
+const amountInput = document.getElementById('amount');                   
+const amountResult = document.getElementById('result');        
+const currencyFromSelect = document.getElementById('currencyFrom');      
+const currencyToSelect = document.getElementById('currencyTo');  
 
+
+
+function getResult() {
+
+  document.getElementById("result").innerHTML = `Result: ${convertAmount()}  ${getSymbol()}`
 }
 
+function getSymbol () {
+return document.getElementById("currencyTo").value
+}
+
+function convertAmount () {
+  const Amount = document.getElementById('amount').value
+  const currentFrom = document.getElementById("currencyFrom").value
+  const currentTo  = document.getElementById("currencyTo").value
+  let result = 0
+
+   if (Amount ==  "" ) {
+    result = 0
+  }
+  
+  else if (currentTo === currentFrom) {
+    result = Amount
+  }
+  
+  
+
+  else if (currentFrom === 'USD') {
+    result =  ( (Amount * exRateUSD[currentTo] * exRateUSD[currentFrom] * 100000) / 100000)
+  }
+  
+  else if (currentFrom === 'EUR') {
+    result =  ( (Amount * exRateEUR[currentTo] * exRateEUR[currentFrom] * 100000) / 100000)
+  }
+
+  else if (currentFrom === 'PLN') {
+    result =  ( (Amount * exRatePLN[currentTo] * exRatePLN[currentFrom] * 100000) / 100000)
+  }
+
+  else if (currentFrom === 'BYN') {
+    result =  ( (Amount * exRateBYN[currentTo] * exRateBYN[currentFrom] * 100000) / 100000)
+  }
+
+  return result
+
+  
+
+
+}
 
